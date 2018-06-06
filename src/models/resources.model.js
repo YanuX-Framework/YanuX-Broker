0092a5688851f5c6410b66fd292d4e234963e0db
petersaints <petersaints@gmail.com>
2018-06-06 19:05:57 +0100

    Changed the Resource "main property" from resource to data. It should be more generic.

--- src/models/resources.model.js
+++ src/models/resources.model.js
@@ -5,13 +5,13 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
+
   const resources = new Schema({
     app: { type: String, required: true },
     user: { type: String, required: true },
-    state: { type: Object, required: true, default: {} }
-  }, {
-      timestamps: true
-    });
+    data: { type: Object, default: {} }
+  }, { timestamps: true });
   resources.index({ app: 1, user: 1 }, { unique: true });
+  
   return mongooseClient.model('resources', resources);
 };
