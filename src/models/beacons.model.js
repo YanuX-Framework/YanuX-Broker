a41f95eb63c4272333f91698a14c8e43c08ad8ec
petersaints <petersaints@gmail.com>
2018-09-20 19:59:02 +0100

    - Improvements to the Beacons service

--- src/models/beacons.model.js
+++ src/models/beacons.model.js
@@ -6,11 +6,14 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const beacons = new Schema({
-    beacon: { type: Object, required: true },
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    beaconKey: {type: String, required: true },
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true },
+    deviceId: {type: String, required: true },
+    beacon: { type: Object, required: true }
   }, {
     timestamps: true
   });
+  beacons.index({beaconKey: 1, deviceId: 1}, {unique: true});
 
   return mongooseClient.model('beacons', beacons);
 };
--- src/services/beacons/beacons.hooks.js
+++ src/services/beacons/beacons.hooks.js
@@ -1,26 +1,69 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+function beforeCreate(context) {
+  if (context.params.user
+    && context.data.beaconKey
+    && context.data.deviceId) {
+    const user = context.params.user;
+    const beaconkey = context.data.beaconKey;
+    const deviceId = context.data.deviceId;
+    return this.remove(null, {
+      query: {
+        user: user._id,
+        deviceId: deviceId,
+        beaconKey: beaconkey
+      }
+    }).then(beacon => {
+      return context;
+    }).catch(e => {
+      throw e;
+    });
+  }
+}
+
+function beforePatchAndUpdate(context) {
+  if (context.data.beacon
+    && context.params.query) {
+    const beacon = context.data.beacon;
+    const query = context.params.query;
+    return this.find({ query: query, paginate: false })
+      .then(beacons => {
+        if (beacons.every(b => beacon.timestamp > b.beacon.timestamp)) {
+          return context;
+        } else {
+          throw new Error('The new beacon\'s timestamp is older than the one(s) already stored.');
+        }
+      })
+      .catch(e => {
+        throw e
+      });
+  }
+}
+
+function afterCreateUpdatePatchAndRemove(context) {
+  console.log('After Create, Update, Patch and Removed Called');
+  // TODO: Implement proxemic notification events.
+}
+
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate('jwt')],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [beforeCreate],
+    update: [beforePatchAndUpdate],
+    patch: [beforePatchAndUpdate],
     remove: []
   },
-
   after: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [afterCreateUpdatePatchAndRemove],
+    update: [afterCreateUpdatePatchAndRemove],
+    patch: [afterCreateUpdatePatchAndRemove],
+    remove: [afterCreateUpdatePatchAndRemove]
   },
-
   error: {
     all: [],
     find: [],
