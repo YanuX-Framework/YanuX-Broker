368e074269682d8243b19726d9dd777edd541bfa
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-02-26 16:16:40 +0000

    - Package Updates

--- src/services/instances/instances.hooks.js
+++ src/services/instances/instances.hooks.js
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const protect = require('@feathersjs/authentication-local').hooks.protect;
