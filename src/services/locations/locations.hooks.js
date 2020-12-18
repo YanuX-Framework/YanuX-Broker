8233fe588b7fba39697167bdb35b8b8e07b6fee6
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-12-18 02:10:52 +0000

    - Some tweaks and bugfixes
    - Package Updates

--- src/services/locations/locations.hooks.js
+++ src/services/locations/locations.hooks.js
@@ -40,7 +40,8 @@ function skipSelfScan(context) {
   } else { return context; }
 }
 
-function updateProxemics(context, user) {
+//TODO: Perhaps I should make a version of this method that works based on a user id instead of locations so that it can be called from other services if needed.
+function updateProxemics(context/*, user*/) {
   if (context.SKIP) { return context; }
 
   if (context.type !== 'after') { throw new GeneralError('This must be run as an \'after\' hook.') }
@@ -74,6 +75,7 @@ function updateProxemics(context, user) {
   const orientationDifference = (o1, o2) => (o2 - o1 + 540) % 360 - 180;
 
   // --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
   //TODO: [UPDATE ABSOLUTE POSITIONING] Code currently disabled because it has not been properly tested/updated.
   /*
   const getCloseDeviceUuids = locationPositions => {
@@ -95,88 +97,92 @@ function updateProxemics(context, user) {
   }
   */
   // --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
 
   const proximityUpdate = (l /* TODO: [UPDATE ABSOLUTE POSITIONING] =>*//*, ignoreAbsolutePositions = false*/) => {
     const now = new Date().getTime();
-    let scanningDevice, detectedDevice, currUser, currProxemics;
+    let scanningDevice, currUser, currProxemics;
     let closeBeaconValues = [];
     return new Promise((resolve, reject) => {
-      Promise.all([
-        context.app.service('devices').find({ query: { $limit: 1, $populate: 'user', deviceUuid: l.deviceUuid } }),
-        context.app.service('devices').find({
-          query: {
-            $limit: 1, $populate: 'user',
-            beaconValues: [l.proximity.beacon.uuid.toLowerCase(), l.proximity.beacon.major, l.proximity.beacon.minor]
+      context.app.service('devices').find({ query: { $limit: 1, $populate: 'user', deviceUuid: l.deviceUuid } })
+        .then(devices => {
+          scanningDevice = (devices.data ? devices.data : devices)[0];
+          if (scanningDevice && scanningDevice.user) {
+            currUser = scanningDevice.user;
+            return context.app.service('proxemics').find({ query: { $limit: 1, user: currUser._id } });
           }
-        })
-      ]).then(devices => {
-        scanningDevice = (devices[0].data ? devices[0].data : devices[0])[0];
-        detectedDevice = (devices[1].data ? devices[1].data : devices[1])[0];
-        if (scanningDevice && scanningDevice.user && detectedDevice) {
-          currUser = scanningDevice.user;
-          return context.app.service('proxemics').find({ query: { $limit: 1, user: currUser._id } });
-        }
-      }).then(proxemics => {
-        if (proxemics) {
-          currProxemics = (proxemics.data ? proxemics.data : proxemics)[0];
-          if (currProxemics && currProxemics.sharedWith && currProxemics.sharedWith.length) {
-            return context.app.service('users').find({ query: { $or: currProxemics.sharedWith.map(u => ({ _id: u })) } });
-          } else { return [] }
-        }
-      }).then(sharedWithUsers => {
-        if (sharedWithUsers) {
-          const users = sharedWithUsers.data ? sharedWithUsers.data : sharedWithUsers;
+        }).then(proxemics => {
+          if (proxemics) {
+            currProxemics = (proxemics.data ? proxemics.data : proxemics)[0];
+            if (currProxemics && currProxemics.sharedWith && currProxemics.sharedWith.length) {
+              return context.app.service('users').find({ query: { _id: { $in: currProxemics.sharedWith } } });
+            } else { return [] }
+          }
+        }).then(result => {
+          if (result) {
+            const users = [currUser._id, ...(result.data ? result.data : result)];
+            return context.app.service('devices').find({ query: { user: { $in: users } } });
+          }
+        }).then(result => {
+          const devices = result.data ? result.data : result;
           return context.app.service('locations').find({
             query: {
-              $or: [{ username: currUser.email }, ...users.map(u => ({ username: u.email }))],
-              proximity: { $exists: true },
+              "proximity.beacon.uuid": { $in: devices.map(d => d.beaconValues[0]) },
+              "proximity.beacon.major": { $in: devices.map(d => d.beaconValues[1]) },
+              "proximity.beacon.minor": { $in: devices.map(d => d.beaconValues[2]) },
               updatedAt: { $gt: now - context.app.get('locations').maxInactivityTime }
             }
           })
-        }
-      }).then(result => {
-        if (result) {
-          const proximityLocations = result.data ? result.data : result;
-          proximityLocations.forEach(pl => {
-            const orientationLocations = proximityLocations.filter(ol => ol.deviceUuid !== pl.deviceUuid);
-            if (pl.proximity.distance < context.app.get('locations').proximityDistanceThreshold &&
-              (orientationLocations.length === 0 || orientationLocations.some(ol => {
-                const orientationDiff = Math.abs(orientationDifference(pl.proximity.orientation, ol.proximity.orientation));
-                return orientationDiff < context.app.get('locations').viewAngleThreshold;
-              }))) { closeBeaconValues.push([pl.proximity.beacon.uuid.toLowerCase(), pl.proximity.beacon.major, pl.proximity.beacon.minor]) }
-          });
-          closeBeaconValues = _.uniqWith(closeBeaconValues, _.isEqual)
-          return closeBeaconValues.length ?
-            context.app.service('devices').find({ query: { $or: closeBeaconValues.length ? closeBeaconValues.map(beaconValues => { return { beaconValues }; }) : [] } }) :
-            Promise.resolve([]);
-        }
-      }).then(result => {
-        if (result) {
-          const devices = result.data ? result.data : result;
-          const proxemics = {
-            //user: currProxemics ? currProxemics.user : currUser || currUser,
-            state: devices.reduce((out, device) => Object.assign(out, { [device.deviceUuid]: device.capabilities }), {})
+        }).then(result => {
+          if (result) {
+            const proximityLocations = result.data ? result.data : result;
+            proximityLocations.forEach(pl => {
+              const orientationLocations = proximityLocations.filter(ol => ol.deviceUuid !== pl.deviceUuid);
+              if (pl.proximity.distance < context.app.get('locations').proximityDistanceThreshold &&
+                (orientationLocations.length === 0 || orientationLocations.some(ol => {
+                  const orientationDiff = Math.abs(orientationDifference(pl.proximity.orientation, ol.proximity.orientation));
+                  return orientationDiff < context.app.get('locations').viewAngleThreshold;
+                }))) { closeBeaconValues.push([pl.proximity.beacon.uuid.toLowerCase(), pl.proximity.beacon.major, pl.proximity.beacon.minor]) }
+            });
+            closeBeaconValues = _.uniqWith(closeBeaconValues, _.isEqual)
+            return closeBeaconValues.length ?
+              context.app.service('devices').find({ query: { beaconValues: { $in: closeBeaconValues } } }) :
+              Promise.resolve([]);
           }
-          return Promise.all([
-            proxemics
+        }).then(result => {
+          if (result) {
+            const devices = result.data ? result.data : result;
+            const proxemics = { state: devices.reduce((out, device) => Object.assign(out, { [device.deviceUuid]: device.capabilities }), {}) }
+            if (!currProxemics || !_.isEqual(currProxemics.state, proxemics.state)) {
+              const users = _.uniq([currUser._id, ...(currProxemics && currProxemics.sharedWith ? currProxemics.sharedWith : [])]);
+              return Promise.all(users.map(u => context.app.service('proxemics').patch(null, proxemics, { query: { user: u } })));
+            }
             // --------------------------------------------------------------------------------
-            //TODO: With the "new" "sharedWith" integration, this may no longer be requirement for "proxemics.find" middleware.
-            /*,_.isEmpty(proxemics.state) ? [] :
-              context.app.service('locations').Model.aggregate([
-                { $match: { $or: Object.keys(proxemics.state).map(deviceUuid => { return { deviceUuid }; }) } },
-                {
-                  $project: {
-                    deviceUuid: '$deviceUuid',
-                    orientation: { $ifNull: ["$position.orientation", "$proximity.orientation"] },
-                    updatedAt: '$updatedAt'
-                  }
-                },
-                { $sort: { "updatedAt": 1 } },
-                { $group: { _id: "$deviceUuid", orientation: { $last: "$orientation" } } }
-              ]),
-              */
+            // --------------------------------------------------------------------------------
+            /*
+            return Promise.all([proxemics,
+              // --------------------------------------------------------------------------------
+              //TODO: With the "new" "sharedWith" integration, this may no longer be requirement for "proxemics.find" middleware.
+              // --------------------------------------------------------------------------------
+              ,_.isEmpty(proxemics.state) ? [] :
+                context.app.service('locations').Model.aggregate([
+                  //TODO: Perhaps replace $or with $in
+                  { $match: { $or: Object.keys(proxemics.state).map(deviceUuid => { return { deviceUuid }; }) } },
+                  {
+                    $project: {
+                      deviceUuid: '$deviceUuid',
+                      orientation: { $ifNull: ["$position.orientation", "$proximity.orientation"] },
+                      updatedAt: '$updatedAt'
+                    }
+                  },
+                  { $sort: { "updatedAt": 1 } },
+                  { $group: { _id: "$deviceUuid", orientation: { $last: "$orientation" } } }
+                ]),
+                */
+            // --------------------------------------------------------------------------------
             //TODO: [UPDATE ABSOLUTE POSITIONING] Code disabled because it is not in use. Moreover, it is not taking into account if locations belong to the same place/room!
             //An aggregation query would probably be needed for that.
+            // --------------------------------------------------------------------------------
             /*
             ignoreAbsolutePositions ? Promise.resolve([]) : context.app.service('locations').find({
               query: {
@@ -184,43 +190,48 @@ function updateProxemics(context, user) {
                 updatedAt: { $gt: now - context.app.get('locations').maxInactivityTime }
               }
             })
-            */
             // --------------------------------------------------------------------------------
-          ])
-        }
-      }).then(result => {
-        if (result) {
-          const [proxemics/*, orientation*/] = result;
-          // --------------------------------------------------------------------------------
-          //TODO: With the "new" "sharedWith" integration, this may no longer be requirement for "proxemics.find" middleware.
-          //In fact, it may be possible to just get a single proxemics from YanuX Coordinator instead of many and then merging them.
-          //orientation.forEach(o => { if (proxemics.state[o._id]) { proxemics.state[o._id]._orientation = o.orientation; } });
-          //TODO: [UPDATE ABSOLUTE POSITIONING] Code currently disabled because it has not been properly tested/updated.
-          /*
-          if (!ignoreAbsolutePositions) {
-            const absolutePositionLocations = result[2].data ? result[2].data : result[2];
-            const currDeviceUuids = getCloseDeviceUuids(absolutePositionLocations);
-            for (const deviceUuid in proxemics.state) {
-              if (absolutePositionLocations.find(al => (al.data ? al.data : al).deviceUuid === deviceUuid)
-                && !currDeviceUuids.find(dUuid => dUuid === deviceUuid)
-              ) { delete proxemics.state[deviceUuid]; }
+          ])*/
+            // --------------------------------------------------------------------------------
+            // --------------------------------------------------------------------------------
+          }
+        })
+        // --------------------------------------------------------------------------------
+        // --------------------------------------------------------------------------------
+        /*.then(result => {
+          if (result) {
+            const [proxemics, orientation*] = result;
+            // --------------------------------------------------------------------------------
+            //TODO: With the "new" "sharedWith" integration, this may no longer be requirement for "proxemics.find" middleware.
+            //In fact, it may be possible to just get a single proxemics from YanuX Coordinator instead of many and then merging them.
+            // --------------------------------------------------------------------------------
+            //orientation.forEach(o => { if (proxemics.state[o._id]) { proxemics.state[o._id]._orientation = o.orientation; } });
+            // --------------------------------------------------------------------------------
+            //TODO: [UPDATE ABSOLUTE POSITIONING] Code currently disabled because it has not been properly tested/updated.
+            // --------------------------------------------------------------------------------
+            /*
+            if (!ignoreAbsolutePositions) {
+              const absolutePositionLocations = result[2].data ? result[2].data : result[2];
+              const currDeviceUuids = getCloseDeviceUuids(absolutePositionLocations);
+              for (const deviceUuid in proxemics.state) {
+                if (absolutePositionLocations.find(al => (al.data ? al.data : al).deviceUuid === deviceUuid)
+                  && !currDeviceUuids.find(dUuid => dUuid === deviceUuid)
+                ) { delete proxemics.state[deviceUuid]; }
+              }
             }
+            // --------------------------------------------------------------------------------
           }
-          */
-          // --------------------------------------------------------------------------------
-          if (!currProxemics || !_.isEqual(currProxemics.state, proxemics.state)) {
-            const sharedWith = currProxemics.sharedWith ? currProxemics.sharedWith : []
-            return context.app.service('proxemics').patch(null, proxemics, {
-              //query: { user: proxemics.user }
-              query: { $or: [{ user: proxemics.user }, ...sharedWith.map(u => ({ user: u })), { sharedWith: { $in: [proxemics.user, ...sharedWith] } }] }
-            })
-          }
-        }
-      }).then(() => { resolve(true); }).catch(e => reject(e));
+        })*/
+        // --------------------------------------------------------------------------------
+        // --------------------------------------------------------------------------------
+        .then(() => { resolve(true); }).catch(e => reject(e));
     });
   }
 
+  // --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
   //TODO: [UPDATE ABSOLUTE POSITIONING] Update the "positionUpdate" function to match the behaviour and improvements made to the "proximityUpdate" function.
+  // --------------------------------------------------------------------------------
   /*
   const positionUpdate = l => {
     const now = new Date().getTime();
@@ -245,6 +256,7 @@ function updateProxemics(context, user) {
             const coLocations = ls.data ? ls.data : ls;
             const currDeviceUuids = getCloseDeviceUuids(coLocations);
             if (currDeviceUuids.length) {
+              //TODO: Perhaps replace $or with $in
               return context.app.service('devices').find({ query: { $or: currDeviceUuids.map(deviceUuid => { return { deviceUuid }; }) } });
             } else { return []; }
           }
@@ -273,17 +285,20 @@ function updateProxemics(context, user) {
     });
   }
   */
+  // --------------------------------------------------------------------------------
 
-  if (user) {
-    //TODO: Make a version of this method that works based on a user/user Id/e-mail instead of location so that it can be called from other services if needed.
-  } else {
-    return Promise.all(locations.map(l => {
-      if (l.proximity) { return proximityUpdate(l); }
-      //TODO: [UPDATE ABSOLUTE POSITIONING] 
-      //else if (l.position) { return positionUpdate(l); }
-      else { throw new GeneralError('Location method currently not supported.') }
-    })).then(() => context).catch(e => { throw e });
-  }
+  /*if (user) {*/
+  // --------------------------------------------------------------------------------
+  //TODO: Perhaps I should make a version of this method that works based on a user id instead of locations so that it can be called from other services if needed.
+  // --------------------------------------------------------------------------------
+  /*} else {*/
+  return Promise.all(locations.map(l => {
+    if (l.proximity) { return proximityUpdate(l); }
+    //TODO: [UPDATE ABSOLUTE POSITIONING] 
+    //else if (l.position) { return positionUpdate(l); }
+    else { throw new GeneralError('Location method currently not supported.') }
+  })).then(() => context).catch(e => { throw e });
+  /*}*/
 }
 
 
