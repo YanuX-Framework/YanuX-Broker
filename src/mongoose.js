20dd211d7d6f6a07d7fc852036f306e3557cd926
Pedro Albuquerque Santos <petersaints@gmail.com>
2021-10-08 19:53:05 +0100

    - Update Packages

--- src/mongoose.js
+++ src/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 module.exports = function (app) {
   mongoose.connect(app.get('mongodb'), {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+    //useNewUrlParser: true,
+    //useCreateIndex: true,
+    //useUnifiedTopology: true
   });
   mongoose.Promise = global.Promise;
 
